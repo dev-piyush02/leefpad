95b2d34fd02d4fcc0f1688bae9ead040e90f98d2
Piyush <mailpiyush02@yahoo.com>
2026-01-06 08:33:32 +0530

    BE url changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = ({ isLoggedIn, onLogout, theme, toggleTheme }) => {
       </div>
       <div className="header-right">
         {isLoggedIn ? (
-          <button className="header-btn" onClick={onLogout}>
+          <button className="header-link" onClick={onLogout}>
             Logout
           </button>
         ) : (
--- src/services/helper.js
+++ src/services/helper.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
-// export const BASE_URL='http://localhost:8080/idle-notes';
-export const BASE_URL='http://localhost:8085/idle-notes';
-//export const BASE_URL='https://leefpad.onrender.com/idle-notes';
+//export const BASE_URL='http://localhost:8085/idle-notes';
+export const BASE_URL = 'https://leefpad-862030982716.europe-west1.run.app/idle-notes';
 
-export const axiosObj= axios.create({
+export const axiosObj = axios.create({
     baseURL: BASE_URL,
 });
